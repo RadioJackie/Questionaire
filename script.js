8be342741e999a36bdacc2a5a0bd009096d78e67
RadioJackie <jacobpeters628@gmail.com>
2020-04-27 22:02:58 -0700

    Got the if else statements to work

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
-var answer = document.querySelector("#card");
-var rightAns = document.querySelector("[name=A1]");
-var wrongAns = document.querySelector("[name=A2]");
+var selectedAns = document.querySelector("#card");
+var rightAns = document.getElementById("A");
+var wrongAns = document.getElementById("B");
+var wrongAns2 = document.getElementById("C");
+var wrongAns3 = document.getElementById("D");
+var card = document.getElementById("Container")
 var mainEl = document.getElementById("main");
 var timerEl = document.getElementById("countdown");
 var bodyEl = document.createElement("div");
 var demoEl = document.getElementById("demo")
-var selectedAns 
 
-       var rightCounter = 0
-       var wrongCounter = 0
+var rightCounter = 0
+var wrongCounter = 0
 
 
-function getAnswer(){
+function getAnswer() {
 
-    if(selectedAns == rightAns){
-        document.querySelector("[name=A1]").innerHTML = "Right!"
+    if (this.value == "Correct") {
+        document.getElementById("A")
         alert("Right!")
     }
-    else {
+    else  {
+        
         alert("Wrong!")
-
     }
-    var wrongAns;
-for (i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
-} 
-    
-}
+    }
 
 rightAns.addEventListener("click", getAnswer)
-    var wrongAns = ["A2", "A2", "A2"];{
-        wrongAns.addEventListener("click", getAnswer)
+wrongAns.addEventListener("click", getAnswer)
+wrongAns2.addEventListener("click", getAnswer)
+wrongAns3.addEventListener("click", getAnswer)
 
-    }
+/*
+var wrongAns = ["B", "C", "D"]; {
+    wrongAns.addEventListener("click", getAnswer)
 
+}
+*/
+seconds = 90;
+var timerEl = setInterval(function () {
 
+})
 
-        
-
-
-    seconds = 90;
-    var timerEl = setInterval(function() {
-        
-    })
-
-    if(seconds<0){
-        clearInterval(timerEl);
-        document.getElementById("demo").innerHTML = "Time";
-    }
-seconds = (seconds-1,1000);
+if (seconds < 0) {
+    clearInterval(timerEl);
+    document.getElementById("demo").innerHTML = "Time";
+}
+seconds = (seconds - 1, 1000);
 
 
 
